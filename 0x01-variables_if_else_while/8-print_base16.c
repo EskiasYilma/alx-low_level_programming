917ecb03cea46b3add14900033e6958eb1c73471
eskiasyilma <eskiasyt@gmail.com>
2022-10-14 16:10:18 +0000

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	char m;
+
+	n = 0;
+	m = 'a';
+
+	while (n < 10)
+	{
+		putchar(n%10 + '0');
+		n++;
+	}
+	while (m <= 'f')
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+
+}
+

e30f28b83d6d28e29b2e57a753ee1457392900ec
eskiasyilma <eskiasyt@gmail.com>
2022-10-14 15:51:33 +0000

    4. alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,8 @@ int main(void)
 	char sletter;
 
 	for (sletter = 'a'; sletter <= 'z'; sletter++)
-	{
-		if (sletter != "e" && sletter != 'q')
+		if (sletter != 'e' && sletter != 'q')
 			putchar(sletter);
-	}
 	putchar('\n');
 	return (0);
 

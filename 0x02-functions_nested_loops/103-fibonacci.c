5226289e2f95f8613601186744f6876a74c5955d
eskiasyilma <eskiasyt@gmail.com>
2022-10-18 17:44:42 +0000

    15. Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,23 +7,24 @@
 
 int main(void)
 {
-	unsigned long int n;
-	unsigned long int m;
-	unsigned long int nth;
-	unsigned long int summ;
+	unsigned long n;
+	unsigned long m;
+	unsigned long nth;
+	unsigned long summ;
 	int counter;
-
-	counter = 0;
+	
+	summ = 0;
 	n = 0;
 	m = 1;
-	while (counter < 50)
+	for (counter = 0; counter < 50; counter++)
 	{
 		nth = n + m;
 		n = m;
 		m = nth;
 		if (nth < 4000000 && nth % 2 == 0)
+		{
 			summ += nth;
-		counter++;
+		}
 	}
 	printf("%lu\n", summ);
 	return (0);

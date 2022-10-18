58616c62e8acf402edfbca6aa6b6cc2de46b7a16
eskiasyilma <eskiasyt@gmail.com>
2022-10-18 19:25:25 +0000

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-	unsigned long int a, b, abth, sumab, d, e, deth, sumde,  next;
+	unsigned long int a, b, abth;
 	int counter;
 
 	counter = 0;
 	a = 0;
 	b = 1;
-	while (counter < 92)
+	while (counter < 98)
 	{
 		abth = a + b;
 		a = b;
@@ -21,29 +21,5 @@ int main(void)
 		printf("%lu, ", abth);
 		counter++;
 	}
-	d = a % 1000;
-	e = b % 1000;
-	a = a / 1000;
-	b = b / 1000;
-	while (counter < 99)
-	{
-		sumde = d + e;
-		sumab = a + b;
-		next = sumde / 1000;
-		deth = sumde - next * 1000;
-		abth = sumab + next;
-		d = e;
-		e = deth;
-		a = b;
-		b = abth;
-		if (deth > 99)
-			printf("%lu%lu", abth, deth);
-		else
-			printf("%lu0%lu", abth, deth);
-		if (counter != 98)
-			printf(", ");
-		counter++;
-	}
-	printf("\n");
 	return (0);
 }

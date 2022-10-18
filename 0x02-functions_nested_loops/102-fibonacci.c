5de6b390384b1a5d8384e6fffd51831a0ddb9192
eskiasyilma <eskiasyt@gmail.com>
2022-10-18 17:19:12 +0000

    14. Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long int n;
+	unsigned long int m;
+	unsigned long int nth;
+	int counter;
+
+	n = 0;
+	m = 1;
+	while (counter < 50)
+	{
+		printf("%lu", n);
+		nth = n + m;
+		n = m;
+		m = nth;
+		counter++;
+	}
+	putchar('\n');
+	return (0);
+}

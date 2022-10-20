b0102c86f50c7d3b229df414c145ffd49b1b744d
eskiasyilma <eskiasyt@gmail.com>
2022-10-20 11:57:26 +0000

    2 multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @n: int to test
+ * @m: int to test
+ * Return: sum of n and m
+ */
+
+int add(int a, int b)
+{
+	return (a * b);
+}

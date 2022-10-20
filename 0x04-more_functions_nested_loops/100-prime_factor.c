c644fc7d01cf1f1aa2a3a833cb961a6ab86dc971
eskiasyilma <eskiasyt@gmail.com>
2022-10-20 17:53:18 +0000

    11. Primenumbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  that finds and prints the largest prime 
+ * factor of the number 612852475143
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	long n;
+	long target;
+
+	target = 612852475143;
+	n = 2;
+	while (n < target)
+	{
+		if (target % n)
+			target = target / n;
+		n++;
+	}
+	printf("%li\n", target);
+	return (0);
+}

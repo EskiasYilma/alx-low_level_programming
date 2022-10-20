bbd56737c5947993dd92660419a141c0247e0730
eskiasyilma <eskiasyt@gmail.com>
2022-10-20 15:00:24 +0000

    9 Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n < 100; n++)
+	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		if (n % 5 == 0)
+		else if (n % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		if (n % 3 == 0)
+		else if (n % 3 == 0)
 		{
 			printf("Fizz");
 		}

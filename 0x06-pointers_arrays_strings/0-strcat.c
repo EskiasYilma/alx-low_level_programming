384a9797dac86688beb73a87f8b144d41de6e49b
eskiasyilma <eskiasyt@gmail.com>
2022-10-25 01:35:08 +0000

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int a = 0;
-	int b = 0;
+	int a, b;
 
-	while (dest[a] != '\0')
-		a++;
-	while (src[b] != '\0')
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
-	dest[a] = '\0';
+	for (a = 0; dest[a] != '\0'; a++)
+	for (b = 0; src[b] != '\0'; b++)
+		dest[a + b] = src[b];
+
+	dest[a + b] = '\0';
 	return (dest);
 }

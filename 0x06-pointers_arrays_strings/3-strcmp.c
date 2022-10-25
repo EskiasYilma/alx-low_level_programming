0db4847ad1b73ef66c55a0a374b4450bc631a4e7
eskiasyilma <eskiasyt@gmail.com>
2022-10-25 01:35:08 +0000

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int a = 0;
 
-	for (a = 0; s1[i] == s2[i]; a++)
+	for (a = 0; s1[a] == s2[a]; a++)
 	{
-		if (s1[i] == '\0')
+		if (s1[a] == '\0')
 			return (0);
 	}
-	return (s1[i] - s2[i]);
+	return (s1[a] - s2[a]);
 }

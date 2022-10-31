b310a9ccbab61e6690a58abc864d790f0a1265be
eskiasyilma <eskiasyt@gmail.com>
2022-10-31 14:35:18 +0000

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,14 @@
 
 unsigned int _strspn(char *s, char *accept);
 {
-	unsigned int len = 0;
-	unsigned int i, j, l1, l2, bool;
+	int count = 0;
+	unsigned int i, j;
+	int bool;
 
-	for (l1 = 0; s[l1] != '\0'; l1++)
-		;
-	for (l2 = 0; accept[l2] != '\0'; l2++)
-		;
-	for (i = 0; i < l1; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		bool = 0;
-		for (j = 0; j < l2; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
 			{
@@ -29,9 +26,9 @@ unsigned int _strspn(char *s, char *accept);
 			}
 		}
 		if (bool == 1)
-			len++;
+			count++;
 		else
 			break;
 	}
-	return (len);
+	return (count);
 }

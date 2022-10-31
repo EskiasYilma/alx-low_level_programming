cff70d66eaf60b58f539402be7fd96541e003504
eskiasyilma <eskiasyt@gmail.com>
2022-10-31 11:47:06 +0000

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search from
+ * @c: character to start searching from
+ * Return: pointer to the first occurence
+ * of the character 'c'
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}

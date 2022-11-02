d4dc302728a7495cb99362ce07b5ff446f27db85
eskiasyilma <eskiasyt@gmail.com>
2022-11-02 18:50:45 +0000

    8. Inception. Now, before you bother telling me it's impossible...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+/**
+ * _sln - get length of a string (abbr for _strlen)
+ * @s: string
+ * Return: length of string 's'
+ */
+
+int _sln(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _sln(++s));
+}
+
+
+/**
+ * wildcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if the strings are identical, else return 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if ((s1[0] != '\0' && s2[0] == '*'))
+	{
+		if ((s1[_sln(s1) - 1] == s2[_sln(s2) - 1]) || (s2[_sln(s2) - 1] == '*'))
+		{
+			return (1);
+		}
+	}
+	if (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 == *s2)
+			return (wildcmp(++s1, ++s2));
+		if (*s1 != *s2 || *s2 != '*')
+			return (0);
+		if (*s1 != *s2 && *s2 == '*')
+			return (wildcmp(++s1, ++s2));
+		return (1);
+	}
+	return (0);
+}

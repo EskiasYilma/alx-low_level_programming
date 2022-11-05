e6ce2a5fc709fd0b60dc710c9944365193b2ba34
eskiasyilma <eskiasyt@gmail.com>
2022-11-05 13:03:30 +0000

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,36 +3,17 @@
 #include <ctype.h>
 
 /**
- * main - prints the minimum number of coins to make change
- * @argc: argument count
- * @argv: argument vector
+ * checker - prints the minimum number of coins to make change
+ * @cents: allowed cents
+ * @sum: sum of cents
  * Return: 0 (success)
  */
 
-int main(int argc, char *argv[])
+int checker(int *cents, int sum)
 {
-	int sum, i, count;
-	int cents[] = {25, 10, 5, 2};
+	int i, count;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	if (atoi(argv[1]) == 1)
-	{
-		printf("%d\n", 1);
-		return (0);
-	}
-	if (atoi(argv[1]) > 0)
-		sum = atoi(argv[1]);
-	else
-	{
-		printf("0\n");
-		return (0);
-	}
 	count = 0;
-
 	if (sum > 1)
 	{
 		while (sum > 1)
@@ -54,7 +35,39 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	printf("%d\n", count);
 	return (0);
+}
+
+
+/**
+ * main - prints the minimum number of coins to make change
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+	int cents[] = {25, 10, 5, 2};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) == 1)
+	{
+		printf("%d\n", 1);
+		return (0);
+	}
+	if (atoi(argv[1]) > 0)
+		sum = atoi(argv[1]);
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+	return (checker(cents, sum));
 }

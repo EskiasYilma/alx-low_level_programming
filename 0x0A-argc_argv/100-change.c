d1368edca7641c36c646275a746a6236fce01f81
eskiasyilma <eskiasyt@gmail.com>
2022-11-05 12:29:10 +0000

    5. Minimal Number of Coins for Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - prints the minimum number of coins to make change
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, i, count;
+	int cents[] = {25, 10, 5, 2}
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	sum = atoi(argv[1]);
+	count = 0;
+
+	if (sum > 1)
+	{
+		while (sum > 1)
+		{
+			for (i = 0; cents[i] != '\0'; i++)
+			{
+				if (sum >= cents[i])
+				{
+					count += sum / cents[i];
+					sum = sum % cents[i];
+				}
+			}
+		}
+		if (sum == 1)
+			count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", count);
+	return (0);
+}
+
+}

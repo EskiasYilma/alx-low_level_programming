e712e5510ba3c5044e9ce20e99c3e0413c0b2392
eskiasyilma <eskiasyt@gmail.com>
2022-11-14 13:46:37 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: Dog's name
+ * @age: Dog's Age
+ * @owner: Dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

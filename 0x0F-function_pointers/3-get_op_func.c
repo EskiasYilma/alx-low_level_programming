5c710c98c0978b882b650fe7ed57f5fdec1b9ab0
eskiasyilma <eskiasyt@gmail.com>
2022-11-16 17:06:48 +0000

    3-fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,9 +24,17 @@ int (*get_op_func(char *s))(int, int)
 
 
 	i = 0;
+	/**
+	 * while (i < 5)
+	 * {
+	 * if (ops[i].op[0] == s[0] && s[1] == '\0')
+	 * return (ops[i].f);
+	 * i++;
+	 * }
+	*/
 	while (i < 5)
 	{
-		if (ops[i].op[0] == s[0] && s[1] == '\0')
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}

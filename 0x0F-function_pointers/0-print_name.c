417d761bd6b83611dc6dfd43460d098c12a53dcc
eskiasyilma <eskiasyt@gmail.com>
2022-11-16 14:42:14 +0000

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,6 @@
 #include "function_pointers.h"
 
 /**
- * f - operation identifier
- * @c: char
- * Return: Nothing
- */
-
-void f(char *c)
-{
-	int i;
-
-	for (i = 0; c[i] != '\0'; i++)
-		_putchar(c[i]);
-}
-
-/**
  * print_name - prints a name
  * @name: name
  * @f: pointer to an operational function

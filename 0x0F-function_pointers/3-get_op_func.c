f0d26561acab0923c0b4e8533df908247fe512b9
eskiasyilma <eskiasyt@gmail.com>
2022-11-16 16:45:26 +0000

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,10 +22,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
+
 	i = 0;
 	while (i < 5)
 	{
-		if (*ops[i].op == *s && !(*(s + 1)))
+		if (ops[i].op == s[0] && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}

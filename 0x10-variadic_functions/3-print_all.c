cd450b4dc1fd8550dfef65c7c072dc64cc10d5fc
eskiasyilma <eskiasyt@gmail.com>
2022-11-17 16:02:36 +0000

    3. To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,89 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * flag_str - string flag
+ * @s: string
+ */
+void flag_str(va_list s)
+{
+	char *str;
+
+	str = va_arg(s, char *);
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * flag_int - integer flag
+ * @i: integer
+ */
+
+void flag_int(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * flag_float - float flag
+ * @f: float
+ */
+
+void flag_float(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * flag_char - character flag
+ * @c: char
+ */
+
+void flag_char(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * print_all - prints anything
+ * @format: flags
+ */
+
+void print_all(const char * const format, ...)
+{
+	char *sep;
+	unsigned int i, j;
+	va_list args;
+
+	v_flags flags[] = {
+		{"c", flag_char},
+		{"i", flag_int},
+		{"f", flag_float},
+		{"s", flag_str},
+		{NULL, NULL}
+	};
+
+	va_start(args, format);
+	i = 0;
+	j = 0;
+	sep = "";
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (flags[i].c_flag[0] == format[j])
+			{
+				printf("%s", sep);
+				flags[i].f(args);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+#include <stddef.h>
+
+/**
+ * struct flags - Struct flags
+ * @c_flag: flag
+ * @f: the function associated
+ */
+
+typedef struct flags
+{
+	char *c_flag;
+	void (*f)();
+} v_flags;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif

d7fa6e82c5070a3a492dd913f68ac66df6aa6195
EskiasYilma <eskiasyt@gmail.com>
2022-11-25 18:14:24 +0300

    fix 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include "lists.h"
 
 void n(void) __attribute__ ((constructor));
+
+/**
+ * n - function to be executed before main
+ */
+
 void n(void)
 {
 	printf("You're beat! and yet, you must allow,\n");

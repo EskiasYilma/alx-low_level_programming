7bd7eedee408fd96c60da744f0333b0ae3123163
EskiasYilma <eskiasyt@gmail.com>
2022-11-25 17:18:38 +0300

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+void n(void) __attribute__ ((constructor));
+void n(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

519a469bba37f16e7bf5bfd346284be7ddc91b08
Eskias Yilma <eskiasy@gmail.com>
2022-11-29 20:54:44 +0300

    101, 102, 103 - 404

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: double pointer to a singly linked list
+ * Return: the number of nodes in the list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	return (0);
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: pointer to the head node of a singly linked list
+ * Return: The address of the node where the loop starts,
+ * or NULL if there is no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	return (NULL);
+}

09a3c7fd045f9a4e7ac382d8c6d727321a593ad0
Eskias Yilma <eskiasy@gmail.com>
2022-12-01 18:07:22 +0300

    2. 10

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ int _strlen(const char *s);
 int char_to_int(char s);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif

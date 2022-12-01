b9150e8e20033f888a0f8ca1b8561599e5017dda
Eskias Yilma <eskiasy@gmail.com>
2022-12-01 18:37:20 +0300

    fix 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,10 +10,18 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int i = sizeof(n) * 8 - 1;
+	unsigned long int j;
 
 	if (index > i)
 		return (-1);
 
-	*n = *n | (1 >> index);
+	j = 1;
+	while (index > 0)
+	{
+		index--;
+		j *= 2;
+	}
+
+	*n += j;
 	return (1);
 }

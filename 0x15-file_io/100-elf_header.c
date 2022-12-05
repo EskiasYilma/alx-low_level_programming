4d75982610c1c308bcdad61ef1c7298803423472
Eskias Yilma <eskiasy@gmail.com>
2022-12-05 20:44:35 +0300

    404 not found

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+#define E_O STDERR_FILENO
 /**
  * main - displays the information contained in the ELF
  * header at the start of an ELF file.
@@ -10,5 +11,12 @@
 
 int main(int __attribute__((__unused__)) argc, char **argv)
 {
+	int fd;
+
+	if (argc != 2)
+		dprintf(E_O, "Usage: elf_header elf_filename\n"), exit(98);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		dprintf(E_O, "Error: Can't read file %s\n", argv[1]), exit(98);
 	return (0);
 }

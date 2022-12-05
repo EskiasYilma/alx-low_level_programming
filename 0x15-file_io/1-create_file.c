b950eb0f20d851f0a134f54583ba0593273b363a
Eskias Yilma <eskiasy@gmail.com>
2022-12-05 18:26:33 +0300

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: pointer to a constant character variable
+ * Return: length of the character variable
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, write_stat, text_len;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	text_len = _strlen(text_content);
+
+	if (access(filename, F_OK) == 0)
+		truncate(filename, text_len);
+
+	write_stat = write(fd, text_content, text_len);
+	if (write_stat == -1)
+		return (-1);
+	if (close(fd) == -1)
+		return (-1);
+	return (1);
+}

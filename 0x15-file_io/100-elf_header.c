a4f4e7c0610fe37d8d263a48d273f75506e1f000
Eskias Yilma <eskiasy@gmail.com>
2022-12-05 20:35:14 +0300

    404 not found

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,8 +3,6 @@
 /**
  * main - displays the information contained in the ELF
  * header at the start of an ELF file.
- * @argc: arguement count
- * @argv: arguement vector
  * Return: 0 (Success)
  */
 

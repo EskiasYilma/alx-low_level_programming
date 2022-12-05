71a54b0782fa9b966296570a1b331651c8e03a4c
Eskias Yilma <eskiasy@gmail.com>
2022-12-05 20:15:39 +0300

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+#define BUFSIZE 1024
+#define E_O STDERR_FILENO
+
 /**
  * main - a program that copies the content of a file to another file.
  * @argc: arguement count
@@ -18,7 +21,6 @@ int main(int argc, char **argv)
 	if (from_file == -1)
 		dprintf(E_O, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	to_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	/* if file exists */
 	if (to_file == -1)
 		dprintf(E_O, "Error: Can't write to %s\n", argv[2]), exit(99);
 

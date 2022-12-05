619fd1f8a570cac889c390ddcc376a4a99a907f8
Eskias Yilma <eskiasy@gmail.com>
2022-12-05 16:50:14 +0300

    0. Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,18 +1,13 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
-int _pow_recursion(int x, int y);
-int _strlen(const char *s);
-int char_to_int(char s);
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif

32c023c034f96372de6f85363bb3661b3c6abf48
Eskias Yilma <eskiasy@gmail.com>
2022-12-05 20:31:19 +0300

    404 not found

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	return (0);
+}

69bef25bf11bd61c284a4133af2d5d7eb53e9d22
Eskias Yilma <eskiasy@gmail.com>
2022-12-05 20:39:24 +0300

    404 not found

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,10 +3,12 @@
 /**
  * main - displays the information contained in the ELF
  * header at the start of an ELF file.
+ * @argc: arguement count
+ * @argv: arguement vector
  * Return: 0 (Success)
  */
 
-int main(void)
+int main(int __attribute__((__unused__)) argc, char **argv)
 {
 	return (0);
 }
